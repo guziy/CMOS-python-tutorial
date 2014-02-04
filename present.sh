4cdaa1a39b3fa69130932b3d3ba1fb842500dc33
guziy <guziy.sasha@gmail.com>
2014-02-04 10:55:23 -0500

    rename presentation files accordingly

--- present.sh
+++ present.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ipython nbconvert Python_CNRCWP_tutorial.ipynb --to slides --post serve --config slides_config.py --template default_transition.tpl
+ipython nbconvert cmos2014-python-tutorial.ipynb --to slides --post serve --config slides_config.py --template default_transition.tpl
