aba479920880c24677d1e4199666d3911a79a051
Oleksandr (Sasha) Huziy <guziy.sasha@gmail.com>
2014-06-04 23:52:46 -0400

    Incorporate changes from Jon

--- present.sh
+++ present.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-ipython nbconvert cmos2014-python-tutorial.ipynb --to slides --post serve --config slides_config.py --template default_transition.tpl
+#ipython nbconvert cmos2014-python-tutorial.ipynb --to slides --post serve --config slides_config.py --template default_transition.tpl
+
+ipython nbconvert cmos2014-python-tutorial-Copy1.ipynb --to slides --post serve --config slides_config.py --template default_transition.tpl
